848d7fdda240a574bcbe9def597e1f50a563c7f3
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2020-02-22 15:45:40 +0000

    created simple login

---
+++ script.js
@@ -0,0 +1,12 @@
+/*  Name        : script.js
+    Author      : Isaac Vander Sluis
+    Description : Handles logic for web app */
+
+function login(form){
+  if (form.email.value == "honeyguy@gmail.com" && form.password.value == "password") {
+    window.location.href = "query.html";
+  } else {
+    alert("Invalid login");
+    form.reset();
+  }
+}
